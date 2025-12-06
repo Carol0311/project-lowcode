f09b75333558f4a86d17e5ffa0a046f639330b98
Carol0311 <18710371681@163.com>
2025-12-06 18:51:07 +0800

    chore(lint): 更新 ESLint 配置以适配新版本- 将配置迁移到 flat 配置格式- 使用 pluginVue.configs['flat/recommended'] 替代 'flat/essential'- 为 Vue 文件禁用 @typescript-eslint/no-unused-vars 规则- 使用新的 ignores 配置替代 globalIgnores

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,3 @@
-import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
@@ -18,9 +17,11 @@ export default defineConfigWithVueTs(
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  {
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
 
-  pluginVue.configs['flat/essential'],
+  pluginVue.configs['flat/recommended'],
   vueTsConfigs.recommended,
 
   {
@@ -50,6 +51,15 @@ export default defineConfigWithVueTs(
         },
       ],
       'vue/multi-word-component-names': 'off',
+    },
+  },
+  {
+    // 针对Vue文件，禁用@typescript-eslint/no-unused-vars
+    // 因为TypeScript ESLint无法识别Vue模板中使用的变量（包括解构变量和组件）
+    // Vue插件的flat/recommended配置已经包含了处理模板变量使用的规则
+    files: ['**/*.vue'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
   skipFormatting,
