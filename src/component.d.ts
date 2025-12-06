700c7597415433d44cc7e1fd8321717390812c60
Carol0311 <18710371681@163.com>
2025-12-06 18:25:28 +0800

    fix(types): 修复 component.d.ts 中的拼写错误
    将 'obejct' 修正为 'object'

--- src/component.d.ts
+++ src/component.d.ts
@@ -1,5 +1,5 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<obejct, object, any>
+  const component: DefineComponent<object, object, any>
   export default component
 }
